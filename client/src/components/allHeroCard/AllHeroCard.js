adb40389bdabcf4a866d131073d58fb80b3e0f71
JacobHocker <jacobdouglas06@gmail.com>
2022-01-15 10:33:52 -0600

    added responsive attributes on hero cards.  made the whole card able to link.  adjusted images file

--- client/src/components/allHeroCard/AllHeroCard.js
+++ client/src/components/allHeroCard/AllHeroCard.js
@@ -5,17 +5,21 @@ import './AllHeroCard.scss';
 function AllHeroCard({ name, heroCardImage, roleCardImage, id}) {
     return(
         <div className="all-hero-single-card">
-                <img src={heroCardImage} alt={name} className='hero-card-image' />
+            <Link className="link" to={`/hero/${id}`}>
+                <div className="hero-card-image-container">
+                    <img src={heroCardImage} alt={name} className='hero-card-image' />
+                </div>
             <div className="all-hero-card-footer">
                 <div className="all-hero-card-role">
                     <img src={roleCardImage} alt={name} className='role-card-image' />
                 </div>  
                 <div className="all-hero-card-name"> 
-                    <Link className="link" to={`/hero/${id}`}>
+                    
                         <h1>{name}</h1>
-                    </Link>
+                    
                 </div>
             </div>
+            </Link>
         </div>
     )
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,47 +6,47 @@ puts
 ###DPS####
 puts "Creating DPS..."
 puts 
-Character.create(name: "Reaper", role: "Damage", hero_card: '/images/Icon-Reaper.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Widowmaker", role: "Damage", hero_card: '/images/Icon-Widowmaker.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Tracer", role: "Damage", hero_card: '/images/Icon-Tracer.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Torbjorn", role: "Damage", hero_card: '/images/Icon-Torbjorn.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Symmetra", role: "Damage", hero_card: '/images/Icon-Symmetra.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Sombra", role: "Damage", hero_card: '/images/Icon-Sombra.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Soldier 76", role: "Damage", hero_card: '/images/Icon-Soldier_76.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Pharah", role: "Damage",hero_card: '/images/Icon-Pharah.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Mei", role: "Damage", hero_card: '/images/Icon-Mei.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Junkrat", role: "Damage", hero_card: '/images/Icon-Junkrat.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Hanzo", role: "Damage", hero_card: '/images/Icon-Hanzo.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Genji", role: "Damage", hero_card: '/images/Icon-Genji.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Echo", role: "Damage", hero_card: '/images/Icon-Echo.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Doomfist", role: "Damage", hero_card: '/images/Icon-Doomfist.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Cassidy", role: "Damage", hero_card: '/images/Icon-Cassidy.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Bastion", role: "Damage", hero_card: '/images/Icon-Bastion.png', role_card: '/images/Icon-Damage.png')
-Character.create(name: "Ashe", role: "Damage", hero_card: '/images/Icon-Ashe.png', role_card: '/images/Icon-Damage.png')
+Character.create(name: "Reaper", role: "Damage", hero_card: '/images/icons/Icon-Reaper.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Widowmaker", role: "Damage", hero_card: '/images/icons/Icon-Widowmaker.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Tracer", role: "Damage", hero_card: '/images/icons/Icon-Tracer.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Torbjorn", role: "Damage", hero_card: '/images/icons/Icon-Torbjorn.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Symmetra", role: "Damage", hero_card: '/images/icons/Icon-Symmetra.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Sombra", role: "Damage", hero_card: '/images/icons/Icon-Sombra.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Soldier 76", role: "Damage", hero_card: '/images/icons/Icon-Soldier_76.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Pharah", role: "Damage",hero_card: '/images/icons/Icon-Pharah.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Mei", role: "Damage", hero_card: '/images/icons/Icon-Mei.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Junkrat", role: "Damage", hero_card: '/images/icons/Icon-Junkrat.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Hanzo", role: "Damage", hero_card: '/images/icons/Icon-Hanzo.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Genji", role: "Damage", hero_card: '/images/icons/Icon-Genji.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Echo", role: "Damage", hero_card: '/images/icons/Icon-Echo.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Doomfist", role: "Damage", hero_card: '/images/icons/Icon-Doomfist.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Cassidy", role: "Damage", hero_card: '/images/icons/Icon-Cassidy.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Bastion", role: "Damage", hero_card: '/images/icons/Icon-Bastion.png', role_card: '/images/icons/Icon-Damage.png')
+Character.create(name: "Ashe", role: "Damage", hero_card: '/images/icons/Icon-Ashe.png', role_card: '/images/icons/Icon-Damage.png')
 
 ###Tanks###
 puts "Creating Tanks..."
 puts 
-Character.create(name: "D.Va", role: "Tank", hero_card: '/images/Icon-D.Va.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Orisa", role: "Tank", hero_card: '/images/Icon-Orisa.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Sigma", role: "Tank", hero_card: '/images/Icon-Sigma.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Roadhog", role: "Tank", hero_card: '/images/Icon-Roadhog.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Reinhardt", role: "Tank", hero_card: '/images/Icon-Reinhardt.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Winston", role: "Tank", hero_card: '/images/Icon-Winston.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Zarya", role: "Tank", hero_card: '/images/Icon-Zarya.png', role_card: '/images/Icon-Tank.png')
-Character.create(name: "Wrecking Ball", role: "Tank", hero_card: '/images/Icon-Wrecking_Ball.png', role_card: '/images/Icon-Tank.png')
+Character.create(name: "D.Va", role: "Tank", hero_card: '/images/icons/Icon-D.Va.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Orisa", role: "Tank", hero_card: '/images/icons/Icon-Orisa.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Sigma", role: "Tank", hero_card: '/images/icons/Icon-Sigma.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Roadhog", role: "Tank", hero_card: '/images/icons/Icon-Roadhog.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Reinhardt", role: "Tank", hero_card: '/images/icons/Icon-Reinhardt.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Winston", role: "Tank", hero_card: '/images/icons/Icon-Winston.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Zarya", role: "Tank", hero_card: '/images/icons/Icon-Zarya.png', role_card: '/images/icons/Icon-Tank.png')
+Character.create(name: "Wrecking Ball", role: "Tank", hero_card: '/images/icons/Icon-Wrecking_Ball.png', role_card: '/images/icons/Icon-Tank.png')
 
 
 ###Supports###
 puts "Creating Supports..."
 puts 
-Character.create(name: "Baptiste", role: "Support", hero_card: '/images/Icon-Baptiste.png', role_card: '/images/Icon-Support.png')
-Character.create(name: "Ana", role: "Support", hero_card: '/images/Icon-Ana.png', role_card: '/images/Icon-Support.png')
-Character.create(name: "Mercy", role: "Support", hero_card: '/images/Icon-Mercy.png', role_card: '/images/Icon-Support.png')
-Character.create(name: "Zenyatta", role: "Support", hero_card: '/images/Icon-Zenyatta.png', role_card: '/images/Icon-Support.png')
-Character.create(name: "Brigitte", role: "Support", hero_card: '/images/Icon-Brigitte.png', role_card: '/images/Icon-Support.png')
-Character.create(name: "Moira", role: "Support", hero_card: '/images/Icon-Moira.png', role_card: '/images/Icon-Support.png')
-Character.create(name: "Lucio", role: "Support", hero_card: '/images/Icon-Lucio.png', role_card: '/images/Icon-Support.png')
+Character.create(name: "Baptiste", role: "Support", hero_card: '/images/icons/Icon-Baptiste.png', role_card: '/images/icons/Icon-Support.png')
+Character.create(name: "Ana", role: "Support", hero_card: '/images/icons/Icon-Ana.png', role_card: '/images/icons/Icon-Support.png')
+Character.create(name: "Mercy", role: "Support", hero_card: '/images/icons/Icon-Mercy.png', role_card: '/images/icons/Icon-Support.png')
+Character.create(name: "Zenyatta", role: "Support", hero_card: '/images/icons/Icon-Zenyatta.png', role_card: '/images/icons/Icon-Support.png')
+Character.create(name: "Brigitte", role: "Support", hero_card: '/images/icons/Icon-Brigitte.png', role_card: '/images/icons/Icon-Support.png')
+Character.create(name: "Moira", role: "Support", hero_card: '/images/icons/Icon-Moira.png', role_card: '/images/icons/Icon-Support.png')
+Character.create(name: "Lucio", role: "Support", hero_card: '/images/icons/Icon-Lucio.png', role_card: '/images/icons/Icon-Support.png')
 
 
 puts "Seeding Complete!"
